a1218ed1e77bd37c183b35627967294096616190
Milena Quittnat <milena.quittnat@cern.ch>
2015-04-29 12:39:29 +0200

    remove all previous crab for tests

--- src/purity_massbin.cpp
+++ src/purity_massbin.cpp
@@ -34,9 +34,11 @@ static char *getDtTm (char *buff) {
 }
 TString date;
 int nbins=0;
+int tnbins=0;
 const int split=2;
 
-int plot_pull(TGraphAsymmErrors* grpull,double dpmass[], double purity[], double w2errL[],double w2errH[],Bool_t truthcheck)  
+//int plot_pull(TGraphAsymmErrors* grpull,double dpmass[], double purity[], double w2errL[],double w2errH[],Bool_t truthcheck)  
+int plot_pull(TGraphAsymmErrors* grpull,double dpmass[], double purity[], double w2err[],Bool_t truthcheck)  
 {
 	  TH1F::SetDefaultSumw2(kTRUE);
 //plot projections-should be gaussian
@@ -54,6 +56,8 @@ int plot_pull(TGraphAsymmErrors* grpull,double dpmass[], double purity[], double
     
 		for(int k=0; k< nbins;k++)
 		{   
+		    ppull[k]=((grpull->GetY()[k])-purity[k])/w2err[k];
+			/*
 			if(purity[k] <= (grpull->GetY()[k]))
 			{
 				ppull[k]=((grpull->GetY()[k])-purity[k])/w2errH[k];
@@ -62,6 +66,7 @@ int plot_pull(TGraphAsymmErrors* grpull,double dpmass[], double purity[], double
 			{
 				ppull[k]=((grpull->GetY()[k])-purity[k])/w2errL[k];
 			}
+			*/
 		    perrxl[k]=grpull->GetErrorXlow(k);
 		    perrxh[k]=grpull->GetErrorXhigh(k);
 		//	cout <<" grpull->GetY()[k] "<< grpull->GetY()[k] << " purity[k] " << purity[k] << " w2errL[k] " << w2errL[k] << " w2errH[k] " << w2errH[k] << " pull[k] " << ppull[k] << " errxl " <<perrxl[k] << " errxh" << perrxh[k] <<endl;
@@ -97,8 +102,8 @@ int plot_pull(TGraphAsymmErrors* grpull,double dpmass[], double purity[], double
     TPad *padpull2 = new TPad("padpull2","",0,0,1,1);
 	padpull2->Draw();
 	padpull2->cd();
-    TH1F* proj=new TH1F("proj","proj",20,-20.,20.);
-    TF1 * g 	 =new TF1("g","gaus",-20.,20.);
+    TH1F* proj=new TH1F("proj","proj",10,-5.,5.);
+    TF1 * g 	 =new TF1("g","gaus",-5.,5.);
     for(int i=0; i<=nbins;i++)
     {
 	   proj->Fill(ppull[i]);
@@ -108,8 +113,10 @@ int plot_pull(TGraphAsymmErrors* grpull,double dpmass[], double purity[], double
 	proj->Fit("g","L ");
 	proj->Draw("HIST");
 	g->Draw("SAME");
-	cpull->Print(Form("%s%s_%s_pull_%s_template_%u_masscut.root",dir2.Data(),(truthcheck)? "truth": "rcone_side",date.Data(),eta,nbins),"root");
-	cpull->Print(Form("%s%s_%s_pull_%s_template_%u_masscut.png",dir2.Data(),(truthcheck)? "truth": "rcone_side",date.Data(),eta,nbins),"png");
+//	cpull->Print(Form("%s%s_%s_pull_%s_template_%u_masscut.root",dir2.Data(),(truthcheck)? "truth": "rcone_side",date.Data(),eta,nbins),"root");
+//	cpull->Print(Form("%s%s_%s_pull_%s_template_%u_masscut.png",dir2.Data(),(truthcheck)? "truth": "rcone_side",date.Data(),eta,nbins),"png");
+	cpull->Print(Form("%s%s_%s_pull_%s_templateunweighted_%u_masscut.root",dir2.Data(),(truthcheck)? "truth": "rcone_side",date.Data(),eta,nbins),"root");
+	cpull->Print(Form("%s%s_%s_pull_%s_templateunweighted_%u_masscut.png",dir2.Data(),(truthcheck)? "truth": "rcone_side",date.Data(),eta,nbins),"png");
 	return 0;
 }
 //TODO etGGa_q definieren
@@ -142,13 +149,13 @@ TGraphAsymmErrors* grcp=NULL;
 
 if(eta_q=="EBEB")
 {
-	fcp=new TFile("../plots/March30/grEB.root","READ");
+	fcp=new TFile("../plots/April8/grEB.root","READ");
 	assert(fcp);
 	grcp=(TGraphAsymmErrors*)fcp->Get("grEB");
 }
 else 	
 {
-	fcp=new TFile("../plots/March30/grnEB.root","READ");
+	fcp=new TFile("../plots/April8/grnEB.root","READ");
 	assert(fcp);
 	grcp=(TGraphAsymmErrors*)fcp->Get("grnEB");
 }
@@ -157,79 +164,89 @@ assert(grcp);
 
 //const char *path=Form("purity_%s_truth_sumw2erron_%s_massbin_20_range",date.Data(),eta);
 //const char *path=Form("purity_2015-03-31_truth_sumw2erron_%s_massbin_20_range",eta);
-const char *path=Form("purity_2015-04-01_truth_sumw2erron_%s_massbin_20_range",eta);
-const char *path2=Form("purity_2015-04-01_truth_sumw2erron_%s_massbin_20_range",eta);
+const char *path=Form("purity_2015-04-08_truth_sumw2erron_%s_massbin_20_range",eta);
+const char *path2=Form("purity_2015-04-23_rcone_sideb_sumw2erron_%s_massbin_20_range",eta);
+//const char *path2=Form("purity_2015-04-08_rcone_sideb_sumw2erron_%s_massbin_20_range",eta);
 //const char *path2=Form("purity_%s_truth_sumw2erron_%s_massbin_20_range",date.Data(),eta);
 //const char *path2=Form("purity_%s_rcone_sideb_sumw2erron_%s_massbin_20_range",date.Data(),eta);
 //const char *path2=Form("purity_2015-03-31_truth_sumw2erron_%s_massbin_20_range",eta);
 //const char *path=Form("purity_2015-03-31_rcone_sideb_sumw2erron_%s_massbin_20_range",eta);
-dir=Form("../plots/April1/%s/massbinned20_4bins_truthreweight/",eta);
+//dir=Form("../plots/April8/%s/massbinned20_4bins_truthreweight/",eta);
+dir=Form("../plots/April8/%s/massbinned20_4bins_truthreweight/",eta);
+dir2=Form("../plots/April17/%s/massbinned20_4bins/",eta);
 //dir=Form("../plots/March31/%s/massbinned20_4bins_reweight/",eta);
 //dir=Form("../plots/March31/%s/massbinned20_4bins_5binspacing_reweight/",eta);
-dir2=Form("../plots/April1/%s/massbinned20_4bins_truthreweight/",eta);
-TGraphAsymmErrors* gr_n1=new TGraphAsymmErrors(); TGraphAsymmErrors* gr_n2=new TGraphAsymmErrors();
-TGraphAsymmErrors* gr_n3=new TGraphAsymmErrors(); TGraphAsymmErrors* gr_n4=new TGraphAsymmErrors();
+//dir2=Form("../plots/April8/%s/massbinned20_4bins_rconesideband/",eta);
+//TGraphAsymmErrors* gr_n1=new TGraphAsymmErrors(); TGraphAsymmErrors* gr_n2=new TGraphAsymmErrors();
+//TGraphAsymmErrors* gr_n3=new TGraphAsymmErrors(); TGraphAsymmErrors* gr_n4=new TGraphAsymmErrors();
+TGraphErrors* gr_n1=new TGraphErrors(); TGraphErrors* gr_n2=new TGraphErrors();
+TGraphErrors* gr_n3=new TGraphErrors(); TGraphErrors* gr_n4=new TGraphErrors();
 int	nbins1=0; int	nbins2=0;
 int	nbins3=0; int	nbins4=0;
+TGraphErrors* gr_tn1=new TGraphErrors(); TGraphErrors* gr_tn2=new TGraphErrors();
+TGraphErrors* gr_tn3=new TGraphErrors(); TGraphErrors* gr_tn4=new TGraphErrors();
+int	nbinst1=0; int	nbinst2=0;
+int	nbinst3=0; int	nbinst4=0;
 //*********************************truthfit***********************************//
 
 //TODO make more flexible for 100 bins 
 //f1=new TFile(Form("%s%s_0_10.root",dir,path),"READ");
-f1=new TFile(Form("%s%s_0_5.root",dir2.Data(),path2),"READ");
+f1=new TFile(Form("%s%s_0_5.root",dir.Data(),path),"READ");
 assert(f1);
-TGraphAsymmErrors* gr1=(TGraphAsymmErrors*)f1->Get("Graph");
+//TGraphAsymmErrors* gratio=(TGraphAsymmErrors*)f1->Get("Graph");
+TGraphErrors* gr1=(TGraphErrors*)f1->Get("Graph");
 assert(gr1); 
 
-f2=new TFile(Form("%s%s_5_10.root",dir2.Data(),path2),"READ");
+f2=new TFile(Form("%s%s_5_10.root",dir.Data(),path),"READ");
 //f2=new TFile(Form("%s%s_10_20.root",dir,path),"READ");
 assert(f2);
-TGraphAsymmErrors* gr2=(TGraphAsymmErrors*)f2->Get("Graph");
+//TGraphAsymmErrors* gr2=(TGraphAsymmErrors*)f2->Get("Graph");
+TGraphErrors* gr2=(TGraphErrors*)f2->Get("Graph");
 assert(gr2);
 
-f3=new TFile(Form("%s%s_10_15.root",dir2.Data(),path2),"READ");
+f3=new TFile(Form("%s%s_10_15.root",dir.Data(),path),"READ");
 assert(f3);
-TGraphAsymmErrors* gr3=(TGraphAsymmErrors*)f3->Get("Graph");
+//TGraphAsymmErrors* gr3=(TGraphAsymmErrors*)f3->Get("Graph");
+TGraphErrors* gr3=(TGraphErrors*)f3->Get("Graph");
 assert(gr3);
-f4=new TFile(Form("%s%s_15_20.root",dir2.Data(),path2),"READ");
+f4=new TFile(Form("%s%s_15_20.root",dir.Data(),path),"READ");
 assert(f4);
-TGraphAsymmErrors* gr4=(TGraphAsymmErrors*)f4->Get("Graph");
+//TGraphAsymmErrors* gr4=(TGraphAsymmErrors*)f4->Get("Graph");
+TGraphErrors* gr4=(TGraphErrors*)f4->Get("Graph");
 assert(gr4);
 
-if(truth)
-{
-	gr_n1=gr1;
-	gr_n2=gr2;
-	gr_n3=gr3;
-	gr_n4=gr4;
-	nbins1=(gr1->GetN());
-	nbins2=(gr2->GetN());
-	nbins3=(gr3->GetN());
-	nbins4=(gr4->GetN());
+	gr_tn1=gr1;
+	gr_tn2=gr2;
+	gr_tn3=gr3;
+	gr_tn4=gr4;
+	nbinst1=(gr1->GetN());
+	nbinst2=(gr2->GetN());
+	nbinst3=(gr3->GetN());
+	nbinst4=(gr4->GetN());
 	cout << "nbins1 truth " << nbins1<< "nbins2 truth" << nbins2<<endl;
-}
+
 //*************************rcone and sideband *********************************//
 f11=new TFile(Form("%s%s_0_5.root",dir2.Data(),path2),"READ");
 //f11=new TFile(Form("%s%s_0_10.root",dir2,path2),"READ");
 assert(f11);
-TGraphAsymmErrors* gr11=(TGraphAsymmErrors*)f11->Get("Graph");
+TGraphErrors* gr11=(TGraphErrors*)f11->Get("Graph");
 assert(gr11);
 f22=new TFile(Form("%s%s_5_10.root",dir2.Data(),path2),"READ");
 //f22=new TFile(Form("%s%s_0_10.root",dir2,path2),"READ");
 assert(f22);
-TGraphAsymmErrors* gr22=(TGraphAsymmErrors*)f22->Get("Graph");
+TGraphErrors* gr22=(TGraphErrors*)f22->Get("Graph");
 assert(gr22);
 
 f33=new TFile(Form("%s%s_10_15.root",dir2.Data(),path2),"READ");
 assert(f33);
-TGraphAsymmErrors* gr33=(TGraphAsymmErrors*)f33->Get("Graph");
+TGraphErrors* gr33=(TGraphErrors*)f33->Get("Graph");
 assert(gr33);
 f44=new TFile(Form("%s%s_15_20.root",dir2.Data(),path2),"READ");
 assert(f44);
-TGraphAsymmErrors* gr44=(TGraphAsymmErrors*)f44->Get("Graph");
+TGraphErrors* gr44=(TGraphErrors*)f44->Get("Graph");
 assert(gr44);
 
-if(!truth)
-{
+
 	gr_n1=gr11;
 	gr_n2=gr22;
 	gr_n3=gr33;
@@ -239,93 +256,124 @@ if(!truth)
 	nbins3=(gr33->GetN());
 	nbins4=(gr44->GetN());
 	cout << "nbins1 rcone sideband " << nbins1<< "nbins2" << nbins2<<endl;
-}
+
 
 
 nbins=nbins1+nbins2+nbins3+nbins4;
+tnbins=nbinst1+nbinst2+nbinst3+nbinst4;
+if(nbins!=nbins) {return -1;}
 cout << "nbins " << nbins << endl;
-//Double_t dpmass_all[nbins];
-//Double_t purity_all[nbins];
-//Double_t w2err_all[nbins];
+double *pdpmasst_all=new double[nbins];
+double *ratio=new double[nbins];
+double *ppurityt_all=new double[nbins];
+double *pw2errt_all=new double[nbins];
 double *pdpmass_all=new double[nbins];
 double *ppurity_all=new double[nbins];
-//double *pw2err_all=new double[nbins];
-double *pw2errL_all=new double[nbins];
-double *pw2errH_all=new double[nbins];
+double *pw2err_all=new double[nbins];
 for(int k=0;k<nbins1; k++ )
 {
 	gr_n1->GetPoint(k,pdpmass_all[k],ppurity_all[k]);
-	pw2errH_all[k]=gr_n1->GetErrorYhigh(k);
-    pw2errL_all[k]=gr_n1->GetErrorYlow(k);
-	cout << "k " << k << "dpmass " << pdpmass_all[k] << "purity " << ppurity_all[k] << "w2errH_all[k] " << pw2errH_all[k] << "w2errL_all[k] " << pw2errL_all[k] <<endl;
+    pw2err_all[k]=gr_n1->GetErrorY(k);
+	cout << "k " << k << "dpmass " << pdpmass_all[k] << "purity " << ppurity_all[k] << "w2err_all[k] " << pw2err_all[k] <<endl;
 }
 //TODO get low and high error
 cout << "second filling " << endl;
 for(int k=0;k<nbins2; k++ )
 {
 	gr_n2->GetPoint(k,pdpmass_all[k+nbins1],ppurity_all[k+nbins1]);
-	pw2errH_all[k+nbins1]=gr_n2->GetErrorYhigh(k);
-	pw2errL_all[k+nbins1]=gr_n2->GetErrorYlow(k);
-	cout << "k " << k+nbins << "dpmass " << pdpmass_all[k+nbins1] << "purity " << ppurity_all[k+nbins1] << "w2errH_all[k] " << pw2errH_all[k+nbins1] << "w2errL_all[k] " << pw2errL_all[k+nbins1] <<endl;
+	pw2err_all[k+nbins1]=gr_n2->GetErrorY(k);
+	cout << "k " << k+nbins << "dpmass " << pdpmass_all[k+nbins1] << "purity " << ppurity_all[k+nbins1] << " w2err_all[k] " << pw2err_all[k+nbins1] <<endl;
 } 
 
 cout << "third filling " << endl;
 for(int k=0;k<nbins3; k++ )
 {
 	gr_n3->GetPoint(k,pdpmass_all[k+nbins1+nbins2],ppurity_all[k+nbins1+nbins2]);
-	pw2errH_all[k+nbins1+nbins2]=gr_n2->GetErrorYhigh(k);
-	pw2errL_all[k+nbins1+nbins2]=gr_n2->GetErrorYlow(k);
-	cout << "k " << k+nbins1+nbins2 << "dpmass " << pdpmass_all[k+nbins1+nbins2] << "purity " << ppurity_all[k+nbins1+nbins2] << "w2errH_all[k] " << pw2errH_all[k+nbins1+nbins2] << "w2errL_all[k] " << pw2errL_all[k+nbins1+nbins2] <<endl;
+//	pw2errH_all[k+nbins1+nbins2]=gr_n2->GetErrorYhigh(k);
+	pw2err_all[k+nbins1+nbins2]=gr_n3->GetErrorY(k);
+	cout << "k " << k+nbins1+nbins2 << "dpmass " << pdpmass_all[k+nbins1+nbins2] << "purity " << ppurity_all[k+nbins1+nbins2] << "w2err_all[k] " << pw2err_all[k+nbins1+nbins2] <<endl;
 } 
 cout << "fourth filling " << endl;
 for(int k=0;k<nbins4; k++ )
 {
 	gr_n4->GetPoint(k,pdpmass_all[k+nbins1+nbins2+nbins3],ppurity_all[k+nbins1+nbins2+nbins3]);
-	pw2errH_all[k+nbins1+nbins2+nbins3]=gr_n4->GetErrorYhigh(k);
-	pw2errL_all[k+nbins1+nbins2+nbins3]=gr_n4->GetErrorYlow(k);
-	cout << "k " << k+nbins1+nbins2+nbins3 << "dpmass " << pdpmass_all[k+nbins1+nbins2+nbins3] << "purity " << ppurity_all[k+nbins1+nbins2+nbins3] << "w2errH_all[k] " << pw2errH_all[k+nbins1+nbins2+nbins3] << "w2errL_all[k] " << pw2errL_all[k+nbins1+nbins2+nbins3] <<endl;
+//	pw2errH_all[k+nbins1+nbins2+nbins3]=gr_n4->GetErrorYhigh(k);
+	pw2err_all[k+nbins1+nbins2+nbins3]=gr_n4->GetErrorY(k);
+	cout << "k " << k+nbins1+nbins2+nbins3 << "dpmass " << pdpmass_all[k+nbins1+nbins2+nbins3] << "purity " << ppurity_all[k+nbins1+nbins2+nbins3]  << "w2err_all[k] " << pw2err_all[k+nbins1+nbins2+nbins3] <<endl;
 } 
+for(int k=0;k<nbinst1; k++ )
+{
+	gr_tn1->GetPoint(k,pdpmasst_all[k],ppurityt_all[k]);
+    pw2errt_all[k]=gr_tn1->GetErrorY(k);
+}
+for(int k=0;k<nbinst2; k++ )
+{
+	gr_tn2->GetPoint(k,pdpmasst_all[k+nbinst1],ppurityt_all[k+nbinst1]);
+	pw2errt_all[k+nbinst1]=gr_tn2->GetErrorY(k);
+} 
+for(int k=0;k<nbinst3; k++ )
+{
+	gr_tn3->GetPoint(k,pdpmasst_all[k+nbinst1+nbinst2],ppurityt_all[k+nbinst1+nbinst2]);
+	pw2errt_all[k+nbinst1+nbinst2]=gr_n3->GetErrorY(k);
+	cout << "k " << k+nbinst1+nbinst2 << "dpmass " << pdpmasst_all[k+nbinst1+nbinst2] << "purity " << ppurityt_all[k+nbinst1+nbinst2] << "w2err_all[k] " << pw2errt_all[k+nbinst1+nbinst2] <<endl;
+} 
+cout << "fourth filling " << endl;
+for(int k=0;k<nbinst4; k++ )
+{
+	gr_tn4->GetPoint(k,pdpmasst_all[k+nbinst1+nbinst2+nbinst3],ppurityt_all[k+nbinst1+nbinst2+nbinst3]);
+	pw2errt_all[k+nbinst1+nbinst2+nbinst3]=gr_tn4->GetErrorY(k);
+	cout << "k " << k+nbinst1+nbinst2+nbinst3 << "dpmass " << pdpmasst_all[k+nbinst1+nbinst2+nbinst3] << "purity " << ppurityt_all[k+nbinst1+nbinst2+nbinst3]  << "w2err_all[k] " << pw2errt_all[k+nbinst1+nbinst2+nbinst3] <<endl;
+}
+for(int k=0; k< nbins ;k++)
+{
+	ratio[k]=(ppurity_all[k]-ppurityt_all[k])/ppurityt_all[k];
+}
 
+TGraphErrors *gratio= new TGraphErrors(nbins,pdpmasst_all,ratio);
+        gratio->SetMarkerStyle(20);
+        gratio->SetTitle("");
 cres->cd();
 
-TPad *pad1 = new TPad("pad1","",0,0,1,1);
+ TPad *pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.0);
+   pad1->SetBottomMargin(0); // Upper and lower plot are joined
+   pad1->SetGridx();         // Vertical grid
+pad1->SetGridy();
 pad1->Draw();
 pad1->cd(); 
-pad1->SetGridx();
-pad1->SetGridy();
 pad1->SetLogx();
+
+
+
+gratio->GetXaxis()->SetTitle("Diphoton mass (GeV)");
 
 grcp->GetYaxis()->SetTitle("purity");
 grcp->GetXaxis()->SetTitle("diphoton mass [GeV]");
-grcp->GetXaxis()->SetLimits(10.,3000.);
-//gr1->GetXaxis()->SetRange(50,3000);
-grcp->GetYaxis()->SetRangeUser(.1,1.);
+//grcp->GetXaxis()->SetLimits(10.,7000.);
+//gratio->GetXaxis()->SetRange(50,3000);
+grcp->GetYaxis()->SetRangeUser(.2,0.9);
 grcp->GetYaxis()->SetTitleOffset(1.2);
  
 grcp->SetMarkerStyle(20);grcp->SetMarkerColor(kBlack); grcp->SetLineColor(kBlack); grcp->SetMarkerSize(1.3);
-gr1->SetMarkerStyle(20);gr1->SetMarkerColor(kRed); gr1->SetLineColor(kRed); gr1->SetMarkerSize(1.3);
-gr2->SetMarkerStyle(20);gr2->SetMarkerColor(kRed); gr2->SetLineColor(kRed); gr2->SetMarkerSize(1.3);
-gr3->SetMarkerStyle(20);gr3->SetMarkerColor(kRed); gr3->SetLineColor(kRed); gr3->SetMarkerSize(1.3);
-gr4->SetMarkerStyle(20);gr4->SetMarkerColor(kRed); gr4->SetLineColor(kRed); gr4->SetMarkerSize(1.3);
-gr11->SetMarkerStyle(20);gr11->SetMarkerColor(kBlue); gr11->SetLineColor(kBlue); gr11->SetMarkerSize(1.3);
-gr22->SetMarkerStyle(20);gr22->SetMarkerColor(kBlue); gr22->SetLineColor(kBlue); gr22->SetMarkerSize(1.3);
-gr33->SetMarkerStyle(20);gr33->SetMarkerColor(kBlue); gr33->SetLineColor(kBlue); gr33->SetMarkerSize(1.3);
-gr44->SetMarkerStyle(20);gr44->SetMarkerColor(kBlue); gr44->SetLineColor(kBlue); gr44->SetMarkerSize(1.3);
+gr1->SetMarkerStyle(20);gr1->SetMarkerColor(kBlue); gr1->SetLineColor(kBlue); gr1->SetMarkerSize(1.3);
+gr2->SetMarkerStyle(20);gr2->SetMarkerColor(kBlue); gr2->SetLineColor(kBlue); gr2->SetMarkerSize(1.3);
+gr3->SetMarkerStyle(20);gr3->SetMarkerColor(kBlue); gr3->SetLineColor(kBlue); gr3->SetMarkerSize(1.3);
+gr4->SetMarkerStyle(20);gr4->SetMarkerColor(kBlue); gr4->SetLineColor(kBlue); gr4->SetMarkerSize(1.3);
+gr11->SetMarkerStyle(20);gr11->SetMarkerColor(kRed); gr11->SetLineColor(kRed); gr11->SetMarkerSize(1.3);
+gr22->SetMarkerStyle(20);gr22->SetMarkerColor(kRed); gr22->SetLineColor(kRed); gr22->SetMarkerSize(1.3);
+gr33->SetMarkerStyle(20);gr33->SetMarkerColor(kRed); gr33->SetLineColor(kRed); gr33->SetMarkerSize(1.3);
+gr44->SetMarkerStyle(20);gr44->SetMarkerColor(kRed); gr44->SetLineColor(kRed); gr44->SetMarkerSize(1.3);
 leg->AddEntry(grcp,Form("truth %s",eta),"p");    
-//leg->AddEntry(gr1,"truth fit EBEB","p");    
-//leg->AddEntry(gr11,"template fit EBEB","p");    
-//leg->AddEntry(gr1,Form("truth fit no reweight %s",eta),"p");    
-leg->AddEntry(gr11,"truth fit EBEB reweighted","p");    
-//leg->AddEntry(gr1,Form("template fit EBEB not reweighted %s",eta),"p");    
+leg->AddEntry(gr1,Form("truth fit rew %s",eta),"p");    
+leg->AddEntry(gr11,Form("template fit not rew%s",eta),"p");    
 grcp->SetTitle(Form("mgg %s purity",eta));
 
 grcp->Draw("AP");
-/*
+
 gr1->Draw("P");
 gr2->Draw(" P");
 gr3->Draw("P");
 gr4->Draw("P");
-*/
+
 gr11->Draw("P");
 gr22->Draw(" P");
 gr33->Draw("P");
@@ -335,17 +383,43 @@ leg->Draw();
 pad1->SetTicks(0,2);
 pad1->Update();
 
+		 
+   TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
+   pad2->SetTopMargin(0);
+   pad2->SetBottomMargin(0.4);
+   pad2->SetTicky();
+   pad2->SetGridx(); // vertical grid
+   pad2->Draw();
+   pad2->cd();
+gratio->Draw("ep");
+gratio->GetYaxis()->SetTitle("(p_temp-p_true)/p_true");
+   gratio->GetYaxis()->SetNdivisions(505);
+   gratio->GetYaxis()->SetTitleSize(18);
+   gratio->GetYaxis()->SetTitleFont(43);
+   gratio->GetYaxis()->SetTitleOffset(1.05);
+   gratio->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+   gratio->GetYaxis()->SetLabelSize(15);
+ // X axis ratio plot settings
+   gratio->GetXaxis()->SetTitleSize(20);
+   gratio->GetXaxis()->SetTitleFont(43);
+   gratio->GetXaxis()->SetTitleOffset(4.);
+   gratio->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+   gratio->GetXaxis()->SetLabelSize(15);
+   //   TLine *r = new TLine(0.,ratio,9.,ratio);
 cres->Update();
-cres->Print(Form("%spurity_%s_%s_template_truth_weightingcomp.root",dir2.Data(),date.Data(),eta),"root");
-cres->Print(Form("%spurity_%s_%s_template_truth_weightingcomp.png",dir2.Data(),date.Data(),eta),"png");
-//cres->Print(Form("%spurity_%s_%s_truth_reweight_comp.root",dir,date.Data(),eta),"root");
-//cres->Print(Form("%spurity_%s_%s_truth_rweweight_comp.png",dir,date.Data(),eta),"png");
-plot_pull(grcp,pdpmass_all,ppurity_all,pw2errL_all,pw2errH_all,truth);
+ 
+
+
+cres->Print(Form("%spurity_%s_%s_templateunweighted_truth_comp.root",dir2.Data(),date.Data(),eta),"root");
+cres->Print(Form("%spurity_%s_%s_templateunweighted_truth_comp.png",dir2.Data(),date.Data(),eta),"png");
+//cres->Print(Form("%spurity_%s_%s_truth_comp.root",dir2.Data(),date.Data(),eta),"root");
+//cres->Print(Form("%spurity_%s_%s_truth_comp.png",dir2.Data(),date.Data(),eta),"png");
+plot_pull(grcp,pdpmass_all,ppurity_all,pw2err_all,truth);
 delete[] pdpmass_all;
 delete[] ppurity_all;
-//delete[] pw2err_all;
-delete[] pw2errL_all;
-delete[] pw2errH_all;
+delete[] pw2err_all;
+//delete[] pw2errL_all;
+//delete[] pw2errH_all;
 
 return 0;
 }
